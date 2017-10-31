abfa95520963f7f0771b016fa80d5e278b996125
Catherine Maldonado <catherine_maldonado@wgbh.org>
2017-10-31 09:41:13 -0400

    removing js

--- save.php
+++ save.php
@@ -6,7 +6,6 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <script src="js/jquery.min.js"></script>
-<script src="js/main.js"></script>
 </script>
 
 <script type="text/javascript" src="https://cdn.emailjs.com/dist/email.min.js"></script>
